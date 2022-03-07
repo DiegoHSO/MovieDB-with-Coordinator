6570e6ace91c29d15105375f2abff07b753d2c60
Diego Henrique Silva Oliveira <53708053+DiegoHSO@users.noreply.github.com>
2022-03-07 13:26:15 -0300

    Initial Commit

---
+++ MovieDB with Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB with Coordinator
+//
+//  Created by Diego Henrique on 07/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
